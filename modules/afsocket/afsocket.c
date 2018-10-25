89e5d330d5e028dbcf3790524814f27e9109e3b2
CodeCracker <antonysu@usc.edu>
2018-10-24 17:24:29 -0700

    Update afsocket.c
    an explanation of how to solve it would be helpful. I had to do a couple of tests to figure things out.
    "tcp windows size per connection=(log_iw_size)/(max-connections). And that a arbitary value for log_iw_size can be mentioned in the source definition where max-connections is mentioned Eg: 'max-connections(5000) log_iw_size(500000)' means a tcp window size per conection is 100 bytes. If it is equal to or more than 100 bytes there won't be any warnings"

--- modules/afsocket/afsocket.c
+++ modules/afsocket/afsocket.c
@@ -612,7 +612,7 @@ afsocket_sd_init(LogPipe *s)
       self->reader_options.super.init_window_size /= self->max_connections;
       if (self->reader_options.super.init_window_size < 100)
         {
-          msg_warning("WARNING: window sizing for tcp sources were changed in syslog-ng 3.3, the configuration value was divided by the value of max-connections(). The result was too small, clamping to 100 entries. Ensure you have a proper log_fifo_size setting to avoid message loss.",
+          msg_warning("WARNING: window sizing for tcp sources were changed in syslog-ng 3.3, the configuration value was divided by the value of max-connections(). tcp windows size per connection=(log_iw_size)/(max-connections). And that a arbitary value for log_iw_size can be mentioned in the source definition where max-connections is mentioned Eg: 'max-connections(5000) log_iw_size(500000)' means a tcp window size per conection is 100 bytes. If it is equal to or more than 100 bytes there won't be any warnings",
                       evt_tag_int("orig_log_iw_size", self->reader_options.super.init_window_size),
                       evt_tag_int("new_log_iw_size", 100),
                       evt_tag_int("min_log_fifo_size", 100 * self->max_connections),
